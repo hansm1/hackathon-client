0649e42406d8ff031b712ddce9f57f73fd518aaa
jconklin19 <156728871+jconklin19@users.noreply.github.com>
2024-01-16 20:59:53 +0000

    Movement Basics

--- sketch.js
+++ sketch.js
@@ -1,7 +1,23 @@
+var x = 200;
+var y = 200;
+var d = 40;
 function setup () {
-  // put setup code here
+  createCanvas(400,400);
 }
 
 function draw () {
-  // put drawing code here
+  background(220);
+  if(keyIsPressed){
+    if(keyCode == LEFT_ARROW){
+      x--;
+    } else if (keyCode == RIGHT_ARROW){
+      x++;
+    }
+    if (keyCode == UP_ARROW){
+      y--;
+    } else if (keyCode == DOWN_ARROW){
+      y++;
+    }
+  }
+  ellipse(x, y, d, d)
 }
