1cd945162f1e2d233a423f3793f5da5a64a7d979
mgillis33 <gillism@emmanuel.edu>
2024-01-16 16:04:41 -0500

    Press to Begin Button

--- sketch.js
+++ sketch.js
@@ -12,7 +12,12 @@ function draw () {
   textSize(50);
   textAlign(CENTER, CENTER);
 
-  rect((width/2)-150,(height/2)-50,300,100,20);
-  
+  fill(242, 185, 11);
+  rect((width/2)-200,(height/2)-50,400,100,20);
+  stroke(255);
+  strokeWeight(2);
+
+  fill(255);
+  text("Press to Begin", width/2, height/2);
   
 }
