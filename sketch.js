c5ef80eb6bba594233d9ab28997cea273fdf597d
Jake Conklin <jakeconklin@Jakes-MacBook-Air.local>
2024-01-16 17:23:25 -0500

    Start Screen

--- sketch.js
+++ sketch.js
@@ -1,12 +1,20 @@
+let img;
+
+function preload () {
+  img = loadImage("PH.png");
+}
+
 function setup () {
   new Canvas()
 }
 
 function draw () {
 
-  background(31, 75, 126);
+  image(img, 0,0, width, height);
+  fill(255,255,255,191);
+  rect((width/2)-250,(height/2)-150,500,300,20)
 
-  fill(242, 185, 11);
+  fill(31, 75, 126);
   text("Emmanuel College", width/2, height/2-100);
   text("Prop Hunt", width/2, height/2+100);
   textSize(50);
@@ -17,7 +25,11 @@ function draw () {
   stroke(255);
   strokeWeight(2);
 
-  fill(255);
+  fill(31, 75, 126);
   text("Press to Begin", width/2, height/2);
+  
+}
+
+function mouseClicked () {
   
 }
