895a2194f2d98ee029fa9e1dfe81c72ecffc7cd4
mgillis33 <gillism@emmanuel.edu>
2024-01-16 15:21:50 -0500

    First real changes

--- sketch.js
+++ sketch.js
@@ -1,7 +1,17 @@
 function setup () {
-  // put setup code here
+  new Canvas()
 }
 
 function draw () {
-  // put drawing code here
+  background(31, 75, 126);
+
+  fill(242, 185, 11);
+  text("Test", width / 2, height / 2);
+  textSize(100);
+  textAlign(CENTER);
+
+  
+  rect(width/2,(height/2)+50,300,100,20);
+
+  
 }
