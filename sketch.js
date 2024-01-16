4c9d30dc6308bc1b1b782a7bd32b7daa37fdf625
Samantha Schumacher <schumachers@emmanuel.edu>
2024-01-16 15:53:28 -0500

    uploading scenemanager library

--- sketch.js
+++ sketch.js
@@ -1,10 +1,15 @@
 var x = 200;
 var y = 200;
 var d = 40;
+var pages = [Quad, JYC]
+
+
 function setup () {
-  // canvas
-  createCanvas(500, 500);
-  fill(150);
+  createCanvas(400, 600);
+  var mgr = new SceneManager();
+    // mgr.bkImage = bkImage; // inject bkImage property
+    mgr.wire();
+    mgr.showScene( Quad );
 }
 
 function draw () {
@@ -23,6 +28,10 @@ function draw () {
   }
   ellipse(x, y, d, d)
 }
+
+// function draw () {
+  
+// }
 
 
 
