6789d0d62b09c53f27499610e7c15b40f1dd857f
Samantha Schumacher <schumachers@emmanuel.edu>
2024-01-16 14:25:08 -0500

    setting up file structures

--- sketch.js
+++ sketch.js
@@ -5,3 +5,11 @@ function setup () {
 function draw () {
   // put drawing code here
 }
+
+
+
+// create an array of all the pages 
+// then create a variable to keep track of what page you're on 
+// identify what areas on the map are 'transporation zones'
+// when the sprite enters a transporation zone, 
+          // alter the 'current' variable to match the corresponding map/page
