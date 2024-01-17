d65a294109f7bc569fa559ab6f608df488781bcf
mgillis33 <gillism@emmanuel.edu>
2024-01-17 10:59:00 -0500

    Home Page first draft done

--- sketch.js
+++ sketch.js
@@ -6,27 +6,34 @@ function preload () {
 
 function setup () {
   new Canvas()
+
+  let gold = color(242, 185, 11);
+  let darkblue = color(31, 75, 126);
+
+  let button = createButton("Press to Begin");
+  button.position((width/2)-200,(height/2)-50);
+  button.size(400,100);
+  button.style('font-size', '50px');
+  button.style('background-color', gold);
+  button.style('color', darkblue);
+  button.style('stroke', darkblue);
+  button.style('font-family', "Pixelify Sans");
+  button.style('border-color', darkblue);
+
 }
 
 function draw () {
 
   image(img, 0,0, width, height);
   fill(255,255,255,191);
-  rect((width/2)-250,(height/2)-150,500,300,20)
+  rect((width/2)-250,(height/2)-125,500,250,20)
 
   fill(31, 75, 126);
-  text("Emmanuel College", width/2, height/2-100);
-  text("Prop Hunt", width/2, height/2+100);
+  text("Emmanuel College", width/2, height/2-75);
+  text("Prop Hunt", width/2, height/2+75);
   textSize(50);
+  textFont("Pixelify Sans");
   textAlign(CENTER, CENTER);
-
-  fill(242, 185, 11);
-  rect((width/2)-200,(height/2)-50,400,100,20);
-  stroke(255);
-  strokeWeight(2);
-
-  fill(31, 75, 126);
-  text("Press to Begin", width/2, height/2);
   
 }
 
