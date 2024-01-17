3dff3c2e9aa13b8c1e6e1efc2c9248991c9c5450
Jake Conklin <jakeconklin@Jakes-MacBook-Air.local>
2024-01-17 11:12:49 -0500

    New Bounds

--- sketch.js
+++ sketch.js
@@ -11,8 +11,8 @@ function setup () {
   let darkblue = color(31, 75, 126);
 
   let button = createButton("Press to Begin");
-  button.position((width/2)-200,(height/2)-50);
-  button.size(400,100);
+  button.position((width/2)-187,(height/2)-25);
+  button.size(375,75);
   button.style('font-size', '50px');
   button.style('background-color', gold);
   button.style('color', darkblue);
@@ -26,10 +26,10 @@ function draw () {
 
   image(img, 0,0, width, height);
   fill(255,255,255,191);
-  rect((width/2)-250,(height/2)-125,500,250,20)
+  rect((width/2)-210,(height/2)-75,420,175)
 
   fill(31, 75, 126);
-  text("Emmanuel College", width/2, height/2-75);
+  text("Emmanuel College", width/2, height/2-50);
   text("Prop Hunt", width/2, height/2+75);
   textSize(50);
   textFont("Pixelify Sans");
