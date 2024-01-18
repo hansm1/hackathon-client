fd589f0c29e00068e1a11f4262ceb6ba654d7943
hjryoung <youngh@emmanuel.edu>
2024-01-18 11:51:36 -0500

    fixed lobby text

--- Lobby.js
+++ Lobby.js
@@ -9,10 +9,10 @@ function Lobby() {
     image(lobby_bg, 0, 0, width, height);
     fill(31, 75, 126);
 
-    textSize(40);
+    textSize(35);
     textFont('Pixelify Sans');
-    text('Emmanuel College', width / 2 - 170, 30);
-    text("Prop Hunt", width / 2 - 100, 60);
+    text('Emmanuel College', width / 2, 30);
+    text("Prop Hunt", width / 2, 60);
 
     textSize(25);
     textFont('Pixelify Sans');
@@ -21,23 +21,22 @@ function Lobby() {
     rect(1, 1, 195, 235);
     rect(width - 195, 1, 195, 235);
     fill('white');
-    text('Rules:', 60, 25);
-    text('Players Joined:', width - 190, 20);
+    text('Rules:', 90, 25);
+    text('Players Joined:', width - 100, 20);
 
     textSize(15);
 
-    const rulesText = `  One player is randomly 
-  assigned as a seeker and
-  the rest are props. Once
-  a prop is [REDACTED], it
-  becomes a  seeker. The 
-  props have 30 seconds to 
-  hide before the seekers 
-  are released. The seekers 
-  have 3 minutes to find the 
-  props before the props 
-  win.`;
-    text(rulesText, -3, 45)
+    const rulesText = `One player is randomly 
+  assigned as a seeker and the 
+  rest are props. Once a prop is 
+  [REDACTED], it becomes a
+  seeker. The props have 30 
+  seconds to hide before the 
+  seekers are released. The 
+  seekers have 3 minutes to 
+  find the props before the 
+  props win.`;
+    text(rulesText, 90, 140)
 
   }
 }
